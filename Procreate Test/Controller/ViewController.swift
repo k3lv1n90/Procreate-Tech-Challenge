1c530faad9ace745d5d0ffb6d53b4660ab66ca62
Kelvin <k3lv1n90@hotmail.com>
2020-09-27 16:27:16 +1000

    Initial Working Filters

--- Procreate Test/Controller/ViewController.swift
+++ Procreate Test/Controller/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var hueSlider: GradientSlider!
     @IBOutlet weak var saturationSlider: GradientSlider!
     @IBOutlet weak var brightnessSlider: GradientSlider!
+    let context = CIContext()
+    var initialImage: UIImage?
+    var currentImage: UIImage?
     
     @IBOutlet weak var resetButton: UIButton!
     @IBOutlet weak var previewButton: UIButton!
@@ -22,12 +25,38 @@ class ViewController: UIViewController {
     @IBOutlet weak var redoButton: UIButton!
     
     @IBOutlet weak var mainView: RoundedView!
+    @IBOutlet weak var imagePreview: UIImageView!
     
     @IBAction func toggleMenu(_ sender: Any) {
         //used in this case just to toggle the ui
         animateColorControlView()
         handleTouch()
     }
+    
+    @IBAction func undoButtonPressed(_ sender: Any) {
+        //go back in time
+    }
+    
+    @IBAction func redoButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func resetButtonPressed(_ sender: Any) {
+        imagePreview.image = initialImage
+        hueSlider.value = 0
+        saturationSlider.value = 1
+        brightnessSlider.value = 0
+        currentImage = nil
+    }
+    
+    @IBAction func previewButtonTouchedDown(_ sender: Any) {
+       //to hide the view
+    }
+    
+    @IBAction func previewButtonTouchUpInside(_ sender: Any) {
+        //to show the view again
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,11 +80,21 @@ extension ViewController {
     
     func configureLayout(){
         mainView.configureView(radius: 20.0)
+        initialImage = imagePreview.image
+        hueSlider.minColor = .blue
         hueSlider.hasRainbow = true
-        hueSlider.minColor = UIColor.blue
+        hueSlider.minimumValue = -1
+        hueSlider.maximumValue = 1
+        hueSlider.value = 0
+        saturationSlider.minimumValue = 0
+        saturationSlider.maximumValue = 2
+        saturationSlider.value = 1
+        brightnessSlider.minimumValue = -1
+        brightnessSlider.maximumValue = 1
+        brightnessSlider.value = 0
         undoButton.isEnabled = false //as at the beginning it has no function since nothing has been changed
         redoButton.isEnabled = false
-        resetButton.isEnabled = false
+        //resetButton.isEnabled = false
     }
     
     func animateColorControlView() {
@@ -65,16 +104,11 @@ extension ViewController {
     
     func handleTouch() {
         hueSlider.actionBlock = {slider, newValue, finished in
-            let brightness = self.brightnessSlider.value
-            let saturation = self.saturationSlider.value
             CATransaction.begin()
             CATransaction.setValue(true, forKey: kCATransactionDisableActions)
-            self.saturationSlider.setGradientVaryingSaturation(hue: newValue, brightness: brightness)
-            self.saturationSlider.thumbColor = UIColor(hue: newValue, saturation: saturation, brightness: brightness, alpha: 1.0)
-            self.brightnessSlider.setGradientVaryingBrightness(hue: newValue, saturation: saturation)
-            self.brightnessSlider.thumbColor = UIColor(hue: newValue, saturation: saturation, brightness: brightness, alpha: 1.0)
-            slider.thumbColor = UIColor(hue: newValue, saturation: saturation, brightness: brightness, alpha: 1.0)
+            self.saturationSlider.setGradientVaryingSaturation(hue: newValue, brightness: 1.0)
             CATransaction.commit()
+            self.applyHue(value: newValue)
         }
         
         saturationSlider.actionBlock = {slider, newValue, finished in
@@ -82,12 +116,9 @@ extension ViewController {
             let hue = self.hueSlider.value
             CATransaction.begin()
             CATransaction.setValue(true, forKey: kCATransactionDisableActions)
-            self.hueSlider.setGradientVaryingHue(saturation: newValue, brightness: brightness)
-            self.hueSlider.thumbColor = UIColor(hue: hue, saturation: newValue, brightness: brightness, alpha: 1.0)
-            self.brightnessSlider.setGradientVaryingBrightness(hue: hue, saturation: newValue)
-            self.brightnessSlider.thumbColor = UIColor(hue: hue, saturation: newValue, brightness: brightness, alpha: 1.0)
-            slider.thumbColor = UIColor(hue: hue, saturation: newValue, brightness: brightness, alpha: 1.0)
             CATransaction.commit()
+            print(newValue)
+            self.applySaturation(value: newValue)
         }
         
         brightnessSlider.actionBlock = {slider, newValue, finished in
@@ -95,12 +126,62 @@ extension ViewController {
             let hue = self.hueSlider.value
             CATransaction.begin()
             CATransaction.setValue(true, forKey: kCATransactionDisableActions)
-            self.saturationSlider.setGradientVaryingHue(saturation: saturation, brightness: newValue)
-            self.saturationSlider.thumbColor = UIColor(hue: hue, saturation: saturation, brightness: newValue, alpha: 1.0)
-            self.hueSlider.setGradientVaryingBrightness(hue: hue, saturation: saturation)
-            self.hueSlider.thumbColor = UIColor(hue: hue, saturation: saturation, brightness: newValue, alpha: 1.0)
-            slider.thumbColor = UIColor(hue: hue, saturation: saturation, brightness: newValue, alpha: 1.0)
             CATransaction.commit()
+            self.applyBrightness(value: newValue)
+        }
+    }
+    
+    func applyHue(value: CGFloat) {
+        DispatchQueue.main.async {
+            let filter = CIFilter(name: "CIHueAdjust")
+            var ciImage : CIImage?
+            if let current = self.currentImage  {
+                ciImage = CIImage(image: current)
+            } else {
+                ciImage = CIImage(image: self.initialImage!)
+            }
+            filter?.setValue(ciImage, forKey: "inputImage")
+            filter?.setValue(Float(value) * Float.pi, forKey: "inputAngle")
+            let result = filter?.outputImage
+            let image = UIImage(cgImage: self.context.createCGImage(result!, from: result!.extent)!)
+            self.imagePreview.image = image
+            self.currentImage = image
+        }
+    }
+    
+    func applySaturation(value: CGFloat) {
+        DispatchQueue.main.async {
+            let filter = CIFilter(name: "CIColorControls")
+            var ciImage : CIImage?
+            if let current = self.currentImage  {
+                ciImage = CIImage(image: current)
+            } else {
+                ciImage = CIImage(image: self.initialImage!)
+            }
+            filter?.setValue(ciImage, forKey: "inputImage")
+            filter?.setValue(Float(value), forKey: "inputSaturation")
+            let result = filter?.outputImage
+            let image = UIImage(cgImage: self.context.createCGImage(result!, from: result!.extent)!)
+            self.imagePreview.image = image
+            self.currentImage = image
+        }
+    }
+    
+    func applyBrightness(value: CGFloat) {
+        DispatchQueue.main.async {
+            let filter = CIFilter(name: "CIColorControls")
+            var ciImage : CIImage?
+            if let current = self.currentImage  {
+                ciImage = CIImage(image: current)
+            } else {
+                ciImage = CIImage(image: self.initialImage!)
+            }
+            filter?.setValue(ciImage, forKey: "inputImage")
+            filter?.setValue(Float(value), forKey: "inputBrightness")
+            let result = filter?.outputImage
+            let image = UIImage(cgImage: self.context.createCGImage(result!, from: result!.extent)!)
+            self.imagePreview.image = image
+            self.currentImage = image
         }
     }
 }
